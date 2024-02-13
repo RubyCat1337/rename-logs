e6abe2b7336de8ed86daca66707ecb032941315b
RubyCat1337 <103893102+RubyCat1337@users.noreply.github.com>
2024-02-13 17:16:20 +0200

    Update rename-log.sh

--- rename-log.sh
+++ rename-log.sh
@@ -1,3 +1,5 @@
-#! /bin/bash
-find . -type f -name '*.log ' | grep -v .do-not-touch | while read fname;
-do echo mv $fname $ {fname/. log/. LOG/}; done | bash -x
+#!/bin/bash
+
+find . -type f -name '*.log' | grep -v '.do-not-touch' | while read -r fname; do
+    echo mv "$fname" "${fname/.log/.LOG}"
+done | bash -x
